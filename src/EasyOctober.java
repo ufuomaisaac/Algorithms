4c2c780c2f291f171e98fcfbc74ab4c07499ef16
Ufuoma Isaac <ejemudaroufuoma@gmail.com>
2025-07-13 23:54:29 +0100

    Added the solution for the Product of Array except self algorithm

--- src/EasyOctober.java
+++ src/EasyOctober.java
@@ -46,4 +46,25 @@ public class EasyOctober {
         }
     }
 
+    //238
+    //Product of Array except self
+    public static int[] productExceptSelf(int[] nums) {
+        int[] res = new int[nums.length];
+        int product = 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            res[i] = product;
+            product *= nums[i];
+        }
+
+        product = 1;
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            res[i] *= product;
+            product *= nums[i];
+        }
+
+        return res;
+    }
+
 }
