8a8222ecf34872935bf01cbc3750c18fdd85c215
Ufuoma Isaac <ejemudaroufuoma@gmail.com>
2025-07-13 23:50:50 +0100

    refactored Rotate Array function

--- src/EasyOctober.java
+++ src/EasyOctober.java
@@ -5,7 +5,6 @@ public class EasyOctober {
     //LEETCODE
     //1962. Remove Stones to Minimize the Total
     //My approach is to apply the rule to the biggest integer in the array removing more value from the largest integer
-
     public static int minStoneSum(int[] piles, int k) {
 
         int totalStone = 0;
@@ -28,26 +27,23 @@ public class EasyOctober {
 
 
 
-
     //LEETCODE
     //189. Rotate Array
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
 
-    class Solution {
-        public void rotate(int[] nums, int k) {
-            k %= nums.length;
-            reverse(nums, 0, nums.length-1);
-            reverse(nums, 0, k-1);
-            reverse(nums, k, nums.length-1);
-        }
-
-        public void reverse(int[] nums, int start, int end){
-            while(start < end){
-                int temp = nums[start];
-                nums[start] = nums[end];
-                nums[end] = temp;
-                start++;
-                end--;
-            }
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
         }
     }
+
 }
