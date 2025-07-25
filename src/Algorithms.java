2f536191ffa738af7c05b424477fb0a206129a1a
Ufuoma Isaac <ejemudaroufuoma@gmail.com>
2025-07-25 20:22:43 +0100

    code refactored

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -138,13 +138,16 @@ public class Algorithms {
     // Leetcode 334.
     // Increasing Triplet Subsequence
     public static boolean increasingTriplett(int[] nums) {
+
         int max1 = Integer.MAX_VALUE;
         int max2 = Integer.MAX_VALUE;
+
         for(int n : nums) {
             if(n <= max1) max1 = n;
             else if(n <= max2) max2 = n;
             else return true;
         }
+
         return false;
     }
 }
