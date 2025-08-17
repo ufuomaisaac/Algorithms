e7c65188a56ebf6119662521598a249aa37ee89f
Ufuoma Isaac <ejemudaroufuoma@gmail.com>
2025-08-17 18:13:13 +0100

    Group anagram

--- src/sunday17th/TwoPointers.java
+++ src/sunday17th/TwoPointers.java
@@ -1,4 +1,25 @@
 package sunday17th;
 
+import java.util.*;
+
 public class TwoPointers {
 }
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String word : strs) {
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+
+            List<String> group = map.getOrDefault(key, new ArrayList<>());
+            group.add(word);
+            map.put(key, group);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
+
