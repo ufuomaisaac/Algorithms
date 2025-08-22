7fe0069e5a22ea13d5f9326a7480b1af6f35d1ba
Ufuoma Isaac <ejemudaroufuoma@gmail.com>
2025-08-22 11:15:20 +0100

    Number of Matching Subsequences

--- src/sunday17th/HashTables.java
+++ src/sunday17th/HashTables.java
@@ -106,6 +106,44 @@ public class HashTables {
         }
     }
 
+
+
+
+    public int numMatchingSubseq(String s, String[] words) {
+
+        Map<String,Integer> map = new HashMap<>();
+        for(String word:words){
+            map.put(word,map.getOrDefault(word,0)+1);
+        }
+
+        int ans = 0;
+        char ch[] = s.toCharArray();
+
+        for(String word:map.keySet()){
+
+            char temp[] = word.toCharArray();
+            int i = 0;
+            int j = 0;
+
+            while(i<ch.length && j<temp.length){
+                if(ch[i]==temp[j]){
+                    i++;
+                    j++;
+                }else{
+                    i++;
+                }
+            }
+
+            if(j==temp.length){
+                ans+=map.get(word);
+            }
+
+        }
+
+        return ans;
+
+    }
+
 }
 
 
