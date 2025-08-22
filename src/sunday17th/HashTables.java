1264fd00a3005fa59914d82482aadd342d3a83e1
Ufuoma Isaac <ejemudaroufuoma@gmail.com>
2025-08-22 23:23:29 +0100

    Number of Good ways to split a string

--- src/sunday17th/HashTables.java
+++ src/sunday17th/HashTables.java
@@ -145,6 +145,32 @@ public class HashTables {
 
     }
 
+
+    //Number of Good ways to split a string
+    // Leetcode 1525
+    public int numSplits(String s) {
+        int n = s.length();
+        int[] prefix = new int[n];
+        int[] suffix = new int[n];
+        Set<Character> pSet = new HashSet<>();
+        Set<Character> qSet = new HashSet<>();
+
+        for (int i = 0; i < n; i++) {
+            pSet.add(s.charAt(i));
+            qSet.add(s.charAt(n - 1 - i));
+            prefix[i] = pSet.size();
+            suffix[n - 1 - i] = qSet.size();
+        }
+
+        int count  = 0;
+        for (int i = 1; i < n; i++) {
+            if (prefix[i - 1] == suffix[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+
 }
 
 
