bd4e2f6e29a37ebf11fa7d0be731ab3bf3f35d00
Ufuoma Isaac <ejemudaroufuoma@gmail.com>
2025-09-13 00:04:53 +0100

    Maximum sum of Distinct Subarrays with Length K

--- src/week2/SlidingWindowFixedSize.java
+++ src/week2/SlidingWindowFixedSize.java
@@ -92,4 +92,69 @@ public class SlidingWindowFixedSize {
 
 
 
+    // Leetcode 567
+    // Permutation in String
+    public boolean checkInclusion(String s1, String s2) {
+        int[] alpha = new int[26];
+        char[] c1 = s1.toCharArray();
+        char[] c2 = s2.toCharArray();
+
+        // Step 1: Count frequency of characters in s1
+        for (int i = 0; i < c1.length; i++) {
+            alpha[c1[i] - 'a'] += 1;
+        }
+
+        int count = 0;
+        for (int right = 0; right < c2.length; right++) {
+            // Step 2: Expand window to include s2[right]
+            if (alpha[c2[right] - 'a'] != 0) {
+                count++;
+                alpha[c2[right] - 'a'] = alpha[c2[right] - 'a'] - 1;
+            } else {
+                // Step 3: If mismatch, shrink window from the left
+                if (count == c1.length)
+                    return true;
+                else {
+                    // Remove leftmost character of the current window
+                    alpha[c2[right - count] - 'a'] = alpha[c2[right - count] - 'a'] + 1;
+                    count--; //length is decreased coz lefft pointer is moved forward one step
+                    right--;  //doing right-- because right++ will be done by for loop I want i to stay where it is
+                }
+            }
+        }
+
+        // Step 4: Check if final window matched s1 length
+        return count == c1.length;
+    }
+
+
+    // LeetCode 2461.
+    // Maximum Sum of Distinct Subarrays With Length K
+    public long maximumSubarraySum(int[] nums, int k) {
+        Map<Integer, Integer> freqMap = new HashMap<>(); // frequency of elements in current window
+        long maxSum = 0, windowSum = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            windowSum += nums[right];
+            freqMap.put(nums[right], freqMap.getOrDefault(nums[right], 0) + 1);
+
+            if (right >= k - 1) {  // window has reached size k
+                if (freqMap.size() == k) {
+                    maxSum = Math.max(maxSum, windowSum);
+                }
+
+                int left = right - k + 1; // left boundary of window
+                windowSum -= nums[left];
+                freqMap.put(nums[left], freqMap.get(nums[left]) - 1);
+                if (freqMap.get(nums[left]) == 0) {
+                    freqMap.remove(nums[left]);
+                }
+            }
+        }
+        return maxSum;
+    }
+
+
+
+
 }
