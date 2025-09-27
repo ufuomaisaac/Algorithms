080833141d11b329044c82d3e00d8ee6a62dd0cc
Ufuoma Isaac <ejemudaroufuoma@gmail.com>
2025-09-27 02:33:07 +0100

    Longest Substring Without Repeating Characters

--- src/week2/SlidingWindowDynamicSIze.java
+++ src/week2/SlidingWindowDynamicSIze.java
@@ -4,9 +4,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class SlidingWindowDynamicSIze {
-}
 
-class Solution {
+    // LeetCode 3
+    // Longest Substring Without Repeating Characters
     public int lengthOfLongestSubstring(String s) {
         Map<Character, Integer> lastSeen =  new HashMap<>();
         int maxLen = 0;
@@ -15,18 +15,19 @@ class Solution {
         for(int end = 0; end < s.length(); end++) {
             char c = s.charAt(end);
 
-        // If we’ve seen this char before and it’s inside the window
-        if (lastSeen.containsKey(c) && lastSeen.get(c) >= start) {
-            start = lastSeen.get(c) + 1; // move left pointer
+            // If we’ve seen this char before and it’s inside the window
+            if (lastSeen.containsKey(c) && lastSeen.get(c) >= start) {
+                start = lastSeen.get(c) + 1; // move left pointer
+            }
+
+            lastSeen.put(c, end); // update last seen index of char
+            maxLen = Math.max(maxLen, end - start + 1);
         }
 
-        lastSeen.put(c, end); // update last seen index of char
-        maxLen = Math.max(maxLen, end - start + 1);
+        return maxLen;
     }
-
-    return maxLen;
 }
 
 
 
-}
+
