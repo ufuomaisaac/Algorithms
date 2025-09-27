96ce4638856e7fa5a0e1f6f824576fe7f696000e
Ufuoma Isaac <ejemudaroufuoma@gmail.com>
2025-09-27 02:30:44 +0100

    lengthOfLongestSubstring

--- src/week2/SlidingWindowDynamicSIze.java
+++ src/week2/SlidingWindowDynamicSIze.java
@@ -1,4 +1,32 @@
 package week2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SlidingWindowDynamicSIze {
+}
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> lastSeen =  new HashMap<>();
+        int maxLen = 0;
+        int start = 0;
+
+        for(int end = 0; end < s.length(); end++) {
+            char c = s.charAt(end);
+
+        // If we’ve seen this char before and it’s inside the window
+        if (lastSeen.containsKey(c) && lastSeen.get(c) >= start) {
+            start = lastSeen.get(c) + 1; // move left pointer
+        }
+
+        lastSeen.put(c, end); // update last seen index of char
+        maxLen = Math.max(maxLen, end - start + 1);
+    }
+
+    return maxLen;
+}
+
+
+
 }
--- src/week2/SlidingWindowFixedSize.java
+++ src/week2/SlidingWindowFixedSize.java
@@ -209,7 +209,6 @@ public class SlidingWindowFixedSize {
                 }
             }
         }
-
         return result;
     }
 }
