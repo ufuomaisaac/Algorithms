a390a2113db69113e4c800b3ddb2999b40ece401
Ufuoma Isaac <ejemudaroufuoma@gmail.com>
2025-09-27 06:04:16 +0100

    characterReplacement

--- src/week2/SlidingWindowDynamicSIze.java
+++ src/week2/SlidingWindowDynamicSIze.java
@@ -26,6 +26,29 @@ public class SlidingWindowDynamicSIze {
 
         return maxLen;
     }
+
+    public int characterReplacement(String s, int k){
+        int left = 0;
+        int maxCount = 0;
+        int result = 0;
+
+        int[] freq = new int[26];
+
+        for(int right = 0; right < s.length(); right++) {
+            char c = s.charAt(right);
+            freq[c - 'A']++;
+            maxCount = Math.max(result, freq[c - 'A']);
+
+            while((right - left + 1) - maxCount > k) {
+                freq[s.charAt(left) - 'A']--;
+                left++;
+            }
+
+            // Update result with the valid window size
+            result = Math.max(result, right - left + 1);
+        }
+        return result;
+    }
 }
 
 
