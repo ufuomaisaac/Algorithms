3b2cfb1bd077976fdb5256ae5f2a22e12b8040bb
Ufuoma Isaac <ejemudaroufuoma@gmail.com>
2025-12-05 22:33:34 +0100

    finish it up later

--- src/week2/SlidingWindowDynamicSIze.java
+++ src/week2/SlidingWindowDynamicSIze.java
@@ -150,12 +150,45 @@ public class SlidingWindowDynamicSIze {
 
     }
 
-
-    //Assignement
+    // Not done
+    // Assignement
     // Do the Sliding window minimum
     public  static int[] minSlidingWindow(int[] nums, int k) {
-        return new int[2];
+
+
+
+
+        //Initialize parameter
+        int n = nums.length;
+        int[] result = new int[n - k + 1];
+        int resultIndex = 0;
+
+
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        for(int i = 0; i < n ; i++) {
+
+            if(!deque.isEmpty() && deque.peekFirst() < i - k + 1) {
+                deque.removeFirst();
+            }
+
+            while(!deque.isEmpty() && deque.peekLast() <= nums[i]) {
+                deque.removeLast();
+            }
+
+            deque.addLast(i);
+
+            if(i >= k - 1){
+                result[resultIndex++] = nums[deque.peekFirst()];
+            }
+
         }
+
+        return result;
+
+
+        }
+
 }
 
 
