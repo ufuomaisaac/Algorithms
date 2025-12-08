1c0f5a11800be101daf5df9a363e3a7996ae7abf
Ufuoma Isaac <ejemudaroufuoma@gmail.com>
2025-12-08 03:43:37 +0100

    longestOnes solution added

--- src/week2/SlidingWindowDynamicSIze.java
+++ src/week2/SlidingWindowDynamicSIze.java
@@ -188,6 +188,31 @@ public class SlidingWindowDynamicSIze {
 
     }
 
+
+    public int longestOnes(int[] nums, int k) {
+
+        int maxCount = 0;
+        int zeroCount = 0;
+        int left = 0;
+
+
+        for(int right = 0; right < nums.length; right++) {
+            if(nums[right] == 0) {
+                zeroCount++;
+            }
+
+            while(zeroCount > k) {
+                if(nums[left] == 0) {
+                    zeroCount--;
+                }
+                left++;
+
+            }
+            maxCount = Math.max(maxCount, right - left + 1);
+        }
+        return maxCount;
+    }
+
 }
 
 
