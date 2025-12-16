56f28a5c47697a2c99a1fa2b0e9b94b99f66bf5d
Ufuoma Isaac <ejemudaroufuoma@gmail.com>
2025-12-16 13:17:05 +0100

    provided solution for maximum Subarray sum problem

--- src/week2/KadanesAlgorithm.java
+++ src/week2/KadanesAlgorithm.java
@@ -9,4 +9,28 @@ public class KadanesAlgorithm {
         int result = Integer.MIN_VALUE;
 
     }
+
+
+    // LeetCode
+    public int maxSubArray(int[] nums) {
+        int maxSum = Integer.MIN_VALUE;
+        int currentSum = 0;
+
+        for(int i = 0; i < nums.length; i++){
+            currentSum += nums[i];
+
+
+            if(currentSum > maxSum){
+                maxSum = currentSum;
+            }
+
+            if(currentSum < 0){
+                currentSum = 0;
+            }
+        }
+
+        return maxSum;
+
+    }
+
 }
