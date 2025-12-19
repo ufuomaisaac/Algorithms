11413f6ab45adb39e054934fab00f90763700f14
Ufuoma Isaac <ejemudaroufuoma@gmail.com>
2025-12-19 07:48:48 +0100

    MatrixIID solution in progress...

--- src/week2/MatrixIID.java
+++ src/week2/MatrixIID.java
@@ -1,4 +1,23 @@
 package week2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MatrixIID {
+
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+        if (matrix == null || matrix.length == 0) return result;
+
+        // 1. Define the four boundaries (walls)
+        int top = 0;
+        int bottom  = matrix.length - 1;
+        int leftColumn = 0;
+        int rightColumn= matrix[0].length - 1;
+
+
+    }
+
+
+
 }
