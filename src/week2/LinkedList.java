79568a37c5cd20d8323876dc80c80ae5cf3b8643
Ufuoma Isaac <ejemudaroufuoma@gmail.com>
2025-12-23 18:59:08 +0100

    added the frame work for Desgin Linkedlist Algorithm

--- src/week2/LinkedList.java
+++ src/week2/LinkedList.java
@@ -1,4 +1,39 @@
 package week2;
 
 public class LinkedList {
+
+    public  MyLinkedList() {
+
+    }
+
+    public int get(int index) {
+
+    }
+
+    public void addAtHead(int val) {
+
+    }
+
+    public void addAtTail(int val) {
+
+    }
+
+    public void addAtIndex(int index, int val) {
+
+    }
+
+    public void deleteAtIndex(int index) {
+
+    }
+
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * MyLinkedList obj = new MyLinkedList();
+ * int param_1 = obj.get(index);
+ * obj.addAtHead(val);
+ * obj.addAtTail(val);
+ * obj.addAtIndex(index,val);
+ * obj.deleteAtIndex(index);
+ */
 }
