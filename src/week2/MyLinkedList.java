722ad529577b01c4ad905bead58733e535fa2c00
Ufuoma Isaac <ejemudaroufuoma@gmail.com>
2025-12-25 01:55:54 +0100

    added the framework for Partition List

--- src/week2/MyLinkedList.java
+++ src/week2/MyLinkedList.java
@@ -141,3 +141,19 @@ public class MyLinkedList {
  * obj.deleteAtIndex(index);
  */
 }
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+
+    }
+}
