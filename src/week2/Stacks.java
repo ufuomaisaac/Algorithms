a5f5e8f667e5a16c891eaa45a1f9efc99eceafbc
Ufuoma Isaac <ejemudaroufuoma@gmail.com>
2025-12-30 03:17:12 +0100

    added the frame work for Min Stack

--- src/week2/Stacks.java
+++ src/week2/Stacks.java
@@ -1,5 +1,37 @@
 package week2;
 
-public class Stacks
-{
+public class Stacks {
 }
+
+class MinStack {
+
+    public MinStack() {
+
+    }
+
+    public void push(int val) {
+
+    }
+
+    public void pop() {
+
+    }
+
+    public int top() {
+
+    }
+
+    public int getMin() {
+
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
+
